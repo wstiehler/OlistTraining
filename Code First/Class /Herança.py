28239f657d62f54b2410eea0f1347e9ad3d2e8f9
William Villani Stiehler <william.stiehler@evoluinfo.com.br>
2021-03-08 20:25:10 -0300

    Upload class Herança and Polimorfismo. Implementation of the note pad

--- Code First/Class /Herança.py
+++ Code First/Class /Herança.py
@@ -1,1 +1,17 @@
-teste
+class Pizza:
+    pedaços = 8
+
+    @classmethod
+    def mudar_tamanho(cls , pedaços):
+        cls.pedaços = pedaços
+
+class Mussarela(Pizza):
+    ...
+
+class Calabresa(Pizza):
+    ...
+
+class MeioAMeio(Mussarela , Calabresa):
+    ...
+
+
--- Code First/Class /Polimorfismo.py
+++ Code First/Class /Polimorfismo.py
@@ -1,1 +1,19 @@
-teste
+class Pizza:
+    pedaços = 8
+
+    @classmethod
+    def mudar_tamanho(cls , pedaços):
+        cls.pedaços = pedaços
+
+    @staticmethod
+    def ingredientes():
+        return 'ingredientes'
+
+
+class Mussarela(Pizza):
+
+    @staticmethod
+    def ingredientes():
+        return ['queijo' ,
+                'azeitona' ,
+                'trigo']
