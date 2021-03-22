61d624acc935c4c168c1947db6cc30e7ac8627b8
William Villani Stiehler <william.stiehler@evoluinfo.com.br>
2021-03-22 13:51:08 -0300

    Upload increment Class Teste_Calculadora

--- CodeFirst/Code First/Class /Teste_Calculadora.py
+++ CodeFirst/Code First/Class /Teste_Calculadora.py
@@ -3,12 +3,19 @@ from decimal import Decimal
 class Calc:
     def soma(self, x, y):
         if isinstance(x, Decimal) and isinstance(y, Decimal):
-            return x + y
+            self.cache = x + y
+            return self.cache
+        elif y is None:
+            return x + self.cache
         else:
             raise Exception('Insira somente números')
 
     def sub(self, x, y):
-        return x -  y
+        if y is None:
+            self.cache = x - self.cache
+        else:
+            self.cache = (x - y)
+            return self.cache
 
     def mult(self, x, y):
         return x * y
@@ -16,11 +23,15 @@ class Calc:
     def div(self, x, y):
         return x / y
 
+    def clear_cache(self):
+        self.cache = 0
+
 class Teste_Calculadora(TestCase):
     def setUp(self):
         self.calc = calc()
 
     def test_soma(self):
+        self.calc.clear_cache()
         self.assertEqual(self.calc.soma(2, 2), 4)
 
     def test_soma_neg(self):
@@ -30,6 +41,7 @@ class Teste_Calculadora(TestCase):
         self.assertEqual(self.calc.soma(2.0, 1.0), 3.0)
 
     def test_sub(self):
+        self.calc.clear_cache()
         self.assertEqual(self.calc.sub(2, 2), 0)
 
     def teste_sub_float(self):
@@ -40,9 +52,11 @@ class Teste_Calculadora(TestCase):
             self.calc.sub('William', 'Villani')
 
     def test_mult(self):
+        self.calc.clear_cache()
         self.assertEqual(self.calc.mult(3, 3), 9)
 
     def teste_div(self):
+        self.calc.clear_cache()
         self.assertEqual(self.calc.div(3, 3), 1)
 
 
