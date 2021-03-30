b4418e8a6d2b95b93739be65ebdfa575a5f82817
William Villani Stiehler <william.stiehler@evoluinfo.com.br>
2021-03-30 11:08:38 -0300

    Upload increment NotePad OlistTraining and class FuncooDir

---
+++ CodeFirst/Code First/Class /Funcao_DIR.py
@@ -0,0 +1,12 @@
+class num:
+    """
+    Classe que simula um objeto inteiro
+    """
+    def __init__(self, numero):
+        self.numero = numero
+
+    def __repr__(self):
+        return 'Num: {}'.format(self.numero)
+
+    def __add__(self, operador):
+        return self.numero + operador 
