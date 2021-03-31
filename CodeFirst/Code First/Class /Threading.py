1653ccdfe5e77be55d377a203350af8329e63aa5
William Villani Stiehler <william.stiehler@evoluinfo.com.br>
2021-03-31 07:39:39 -0300

    Upload increment of the class Threading

--- CodeFirst/Code First/Class /Threading.py
+++ CodeFirst/Code First/Class /Threading.py
@@ -3,20 +3,44 @@ from time import sleep
 
 
 def wait():
-    count = 0
-    while True:
-        print(count)
-        count +=1
-        sleep(0.1)
+    sleep(2)
+    print('Acabou')
 
 
-t = threading.Thread(target=wait, name='wait', daemon=True)
+class myThread(threading.Thread):
+    def __init__(self, target, name='myThread'):
+        super().__init__()
+        self.target = target
+        self.name = name
+
+    def run(self):
+        self.target()
+
+
+t = myThread(wait)
 t.start()
-print(threading.enumerate()[1].is_alive())#Se esta ativa. Retorna true ou False
-sleep(1)
-print(t.is_alive())
+print(t.name)
 
 
+
+
+
+
+
+
+
+
+#Funciona uma "agrarrando" a outra.
+#t1 = threading.Thread(target=wait, name='wait', daemon=True)
+#t1.start()
+#print(t1.is_alive())
+#print(t1.name)
+
+
+#t2 = threading.Thread(target=wait, name='wait')
+#t2.start()
+#print(t2.is_alive())
+#print(t2.name)
 
 
 
